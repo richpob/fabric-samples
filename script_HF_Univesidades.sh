614c47d3f0697252b890fdabb3b09d3c77155103
richpob <133718913+richpob@users.noreply.github.com>
2024-06-24 18:58:43 -0400

    Funcionando
    Se cambia dominio universidades.com por universidadesg7.com error de sitio publico y certificados

--- script_HF_Univesidades.sh
+++ script_HF_Univesidades.sh
@@ -1,3 +1,70 @@
+#FIN.......................
+
+INSTALAR RED UNIVERSITARIA
+
+--> Borrar instalaciones previas
+cd ~/Documents/GitHub/hyperledger-fabric/fabric-samples/universidades_g7
+./network.sh down
+
+cd ~/Documents/GitHub/hyperledger-fabric/
+
+#git clone https://gitlab.com/STorres17/soluciones-blockchain.git
+
+cd ~/Documents/GitHub/hyperledger-fabric/fabric-samples/universidades_g7
+
+#sudo apt install jq
+
+#docker stop $(docker ps -a -q)
+docker ps -q | xargs -r docker stop
+#docker rm $(docker ps -a -q)
+docker ps -q | xargs -r docker rm
+
+docker volume prune 
+docker network prune
+
+rm -rf organizations/peerOrganizations
+rm -rf organizations/ordererOrganizations
+rm -rf channel-artifacts/
+mkdir channel-artifacts
+
+export PATH=${PWD}/../bin:${PWD}:$PATH
+export FABRIC_CFG_PATH=${PWD}/../config
+cryptogen generate --config=./organizations/cryptogen/crypto-config-iebs.yaml --output="organizations"
+cryptogen generate --config=./organizations/cryptogen/crypto-config-cantabria.yaml --output="organizations"
+cryptogen generate --config=./organizations/cryptogen/crypto-config-orderer.yaml --output="organizations"
+
+docker-compose -f docker/docker-compose-universidades.yaml up -d
+
+//ya tenemos arracanda la red con 1 orderer, 2 orgs y 2 couchdbs 
+//habrá que crear los canales
+
+export FABRIC_CFG_PATH=${PWD}/configtx
+configtxgen -profile UniversidadesGenesis -outputBlock ./channel-artifacts/universidadeschannel.block -channelID universidadeschannel
+export FABRIC_CFG_PATH=${PWD}/../config
+export ORDERER_CA=${PWD}/organizations/ordererOrganizations/universidadesg7.com/orderers/orderer.universidadesg7.com/msp/tlscacerts/tlsca.universidadesg7.com-cert.pem
+export ORDERER_ADMIN_TLS_SIGN_CERT=${PWD}/organizations/ordererOrganizations/universidadesg7.com/orderers/orderer.universidadesg7.com/tls/server.crt
+export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/ordererOrganizations/universidadesg7.com/orderers/orderer.universidadesg7.com/tls/server.key
+
+osnadmin channel join --channelID universidadeschannel --config-block ./channel-artifacts/universidadeschannel.block -o localhost:7053 --ca-file "$ORDERER_CA" --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY"
+osnadmin channel list -o localhost:7053 --ca-file "$ORDERER_CA" --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY"
+
+export CORE_PEER_TLS_ENABLED=true
+export PEER0_IEBS_CA=${PWD}/organizations/peerOrganizations/iebs.universidadesg7.com/peers/peer0.iebs.universidadesg7.com/tls/ca.crt
+export CORE_PEER_LOCALMSPID="IebsMSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_IEBS_CA
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/iebs.universidadesg7.com/users/Admin@iebs.universidadesg7.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+peer channel join -b ./channel-artifacts/universidadeschannel.block
+
+export PEER0_CANTABRIA_CA=${PWD}/organizations/peerOrganizations/cantabria.universidadesg7.com/peers/peer0.cantabria.universidadesg7.com/tls/ca.crt
+export CORE_PEER_LOCALMSPID="CantabriaMSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_CANTABRIA_CA
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/cantabria.universidadesg7.com/users/Admin@cantabria.universidadesg7.com/msp
+export CORE_PEER_ADDRESS=localhost:9051
+peer channel join -b ./channel-artifacts/universidadeschannel.block
+
+
+
 # INSTALAR RED UNIVERSITARIA
 # --> Borrar instalaciones previas
 cd ~/Documents/GitHub/hyperledger-fabric/fabric-samples/universidades_g7
@@ -74,67 +141,6 @@ export CORE_PEER_ADDRESS=localhost:9051
 peer channel join -b ./channel-artifacts/universidadeschannel.block
 # --> REVISAR DOCKERS Y SUS LOGS
 
-#FIN.......................
-
-INSTALAR RED UNIVERSITARIA
-
---> Borrar instalaciones previas
-cd cd ~/Documents/GitHub/hyperledger-fabric/fabric-samples/universidades_g7
-./network.sh down
-
-cd ~/Documents/GitHub/hyperledger-fabric/
-
-git clone https://gitlab.com/STorres17/soluciones-blockchain.git
-
-cd ~/Documents/GitHub/hyperledger-fabric/fabric-samples/universidades_g7
-
-#sudo apt install jq
-
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker volume prune
-docker network prune
-
-rm -rf organizations/peerOrganizations
-rm -rf organizations/ordererOrganizations
-rm -rf channel-artifacts/
-mkdir channel-artifacts
-
-export PATH=${PWD}/../bin:${PWD}:$PATH
-export FABRIC_CFG_PATH=${PWD}/../config
-cryptogen generate --config=./organizations/cryptogen/crypto-config-madrid.yaml --output="organizations"
-cryptogen generate --config=./organizations/cryptogen/crypto-config-bogota.yaml --output="organizations"
-cryptogen generate --config=./organizations/cryptogen/crypto-config-orderer.yaml --output="organizations"
-
-docker-compose -f docker/docker-compose-universidades.yaml up -d
-
-//ya tenemos arracanda la red con 1 orderer, 2 orgs y 2 couchdbs
-//habrá que crear los canales
-
-export FABRIC_CFG_PATH=${PWD}/configtx
-configtxgen -profile UniversidadesGenesis -outputBlock ./channel-artifacts/universidadeschannel.block -channelID universidadeschannel
-export FABRIC_CFG_PATH=${PWD}/../config
-export ORDERER_CA=${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/msp/tlscacerts/tlsca.universidades.com-cert.pem
-export ORDERER_ADMIN_TLS_SIGN_CERT=${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/tls/server.crt
-export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/tls/server.key
-
-osnadmin channel join --channelID universidadeschannel --config-block ./channel-artifacts/universidadeschannel.block -o localhost:7053 --ca-file "$ORDERER_CA" --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY"
-osnadmin channel list -o localhost:7053 --ca-file "$ORDERER_CA" --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY"
-
-export CORE_PEER_TLS_ENABLED=true
-export PEER0_MADRID_CA=${PWD}/organizations/peerOrganizations/madrid.universidades.com/peers/peer0.madrid.universidades.com/tls/ca.crt
-export CORE_PEER_LOCALMSPID="MadridMSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_MADRID_CA
-export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/madrid.universidades.com/users/Admin@madrid.universidades.com/msp
-export CORE_PEER_ADDRESS=localhost:7051
-peer channel join -b ./channel-artifacts/universidadeschannel.block
-
-export PEER0_BOGOTA_CA=${PWD}/organizations/peerOrganizations/bogota.universidades.com/peers/peer0.bogota.universidades.com/tls/ca.crt
-export CORE_PEER_LOCALMSPID="BogotaMSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_BOGOTA_CA
-export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/bogota.universidades.com/users/Admin@bogota.universidades.com/msp
-export CORE_PEER_ADDRESS=localhost:9051
-peer channel join -b ./channel-artifacts/universidadeschannel.block
 
 --> REVISAR DOCKERS Y SUS LOGS
 # ------------------ ADICIÓN DE UNA ORGANIZACIÓN A LA RED UNIVERSITARIA ------------------
